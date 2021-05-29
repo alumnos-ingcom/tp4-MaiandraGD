53d67d24098a7c93d02213fd909c8e0c211e17aa
Yuli <pinchabon@gmail.com>
2021-05-29 20:29:31 -0300

    Subida EJ2

---
+++ tp4-ej2.py
@@ -0,0 +1,14 @@
+def suma_lenta():
+    num1 = int(input("Ingrese el primero numero: "))
+    num2 = int(input("Ingrese el segundo numero: "))
+    
+    while num2 > 0:
+        num1 = num1 + 1
+        num2 = num2 - 1
+    print(num1)
+    
+if __name__ == "__main__":
+    suma_lenta()
+
+# no funciona si el segundo numero es negativo
+# podría probar con valor absoluto o un condicional
